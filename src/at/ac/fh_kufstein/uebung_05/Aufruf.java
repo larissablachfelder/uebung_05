8a5d7ec36c2b591894d568969be8892348b87568
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-12-17 15:14:20 +0100

    aufgabe 7

--- src/at/ac/fh_kufstein/uebung_05/Aufruf.java
+++ src/at/ac/fh_kufstein/uebung_05/Aufruf.java
@@ -1,21 +1,39 @@
 package at.ac.fh_kufstein.uebung_05;
 
 import at.ac.fh_kufstein.uebung_05.Classes.Auto;
+import at.ac.fh_kufstein.uebung_05.Classes.Fahrzeug;
 import at.ac.fh_kufstein.uebung_05.Classes.Wasserfahrzeug;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Aufruf
 {
     public static void main(String[] args)
     {
         //aufgabe 5
-        Auto bmw = new Auto((short)4, (short)4, "silber", (short)220, (short)5, (short)0);
-        Auto audi = new Auto((short)8, (short)4, "schwarz", (short)180, (short)5, (short)0);
-        Wasserfahrzeug titanic = new Wasserfahrzeug(10.54, (short)3, (short)1000000,(short)0, "blau", (short)51000, (short)0, (short)0);
+        Auto bmw = new Auto((short) 4, (short) 4, "silber", (short) 220, (short) 5, (short) 0);
+        Auto audi = new Auto((short) 8, (short) 4, "schwarz", (short) 180, (short) 5, (short) 0);
+        Wasserfahrzeug titanic = new Wasserfahrzeug(10.54, (short) 3, (short) 1000000, (short) 0, "blau", (short) 51000, (short) 0, (short) 0);
 
         //aufgabe 6
         System.out.println(bmw);
         System.out.println(audi);
         System.out.println(titanic);
+
+        //aufgabe 7
+
+        List<Fahrzeug> autoListe = new ArrayList<>();
+
+        autoListe.add(bmw);
+        autoListe.add(audi);
+        autoListe.add(titanic);
+
+
+        for (Fahrzeug item : autoListe) {
+            System.out.println(item);
+
+        }
 
     }
 }
